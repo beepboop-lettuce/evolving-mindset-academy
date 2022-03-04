70d9ff7e696ee04bddc419580aff97d327512f30
beepboop-lettuce <ericaharris0486@gmail.com>
2022-03-04 11:17:12 -0600

    setting up http server

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	//accepts a path and a function as arguments
+	//handler function has to have appropriate signature (as described by "handler" func below)
+	http.HandleFunc("/", handler)
+
+	//after defining server, listen and serve on port 8080
+	//second arg=handler--left as nil for now
+	//handler defined above is used
+	http.ListenAndServe(":8080", nil)
+
+}
+
+//handler= handler func. Follows func signature of ResponseWriter and Request type
+// as the args.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
