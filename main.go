b03a590a66d19cdbb15d0da017e64f20260a260a
beepboop-lettuce <ericaharris0486@gmail.com>
2022-03-08 09:46:54 -0600

    built form in index.html, working on submitting form data

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+type formContent struct {
+	firstName  string
+	lastName   string
+	email      string
+	phone      string
+	optInEmail bool
+	optInText  bool
+}
 
 //new router func creates router and returns it.
 //can use this func to instantiate and test router outside main func
@@ -18,8 +28,27 @@ func newRouter() *mux.Router {
 	//declare handler- routes requests to respective filename
 	//fileserver wrapped in the `stripPrefix` method--want to remove "/assets/" prefix when looking for files
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	//declare form handler- client form submissions from website
+	formHandler := func(rw http.ResponseWriter, req *http.Request) {
+		if err := req.ParseForm(); err != nil {
+			http.Error(rw, "Could not parse form", http.StatusBadRequest)
+		}
+		oie, _ := strconv.ParseBool(req.PostForm.Get("optInEmail"))
+		oit, _ := strconv.ParseBool(req.PostForm.Get("optInText"))
+		content := formContent{
+			firstName:  req.PostForm.Get("firstName"),
+			lastName:   req.PostForm.Get("lastName"),
+			email:      req.PostForm.Get("email"),
+			phone:      req.PostForm.Get("phone"),
+			optInEmail: oie,
+			optInText:  oit,
+		}
+		fmt.Println(content)
+	}
 	//"PathPrefix" method = matcher, matches all routes starting with "/asses/" instead of absolute route
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	r.HandleFunc("/form/", formHandler).Methods("POST")
 	return r
 }
 
